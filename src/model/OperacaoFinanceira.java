a724f077bea86188eea1a8b11f8e64f3cba4da36
Ketlyn Santos <ketlyncastro2004@gmail.com>
2026-05-17 22:40:24 -0300

    feat: cria classe abstrata OperacaoFinanceira

---
+++ src/model/OperacaoFinanceira.java
@@ -0,0 +1,63 @@
+package model;
+
+import enums.StatusOrdem;
+import enums.TipoOperacao;
+
+import java.time.LocalDateTime;
+
+public abstract class OperacaoFinanceira {
+    protected int id;
+    protected TipoOperacao tipoOperacao;
+    protected double quantidade;
+    protected Criptoativo criptoativo;
+    protected LocalDateTime dataOperacao;
+
+    public OperacaoFinanceira(TipoOperacao tipoOperacao, double quantidade, Criptoativo criptoativo) {
+        this.tipoOperacao = tipoOperacao;
+        this.quantidade = quantidade;
+        this.criptoativo = criptoativo;
+        this.dataOperacao = LocalDateTime.now();
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public TipoOperacao getTipoOperacao() {
+        return tipoOperacao;
+    }
+
+    public double getQuantidade() {
+        return quantidade;
+    }
+
+    public Criptoativo getCriptoativo() {
+        return criptoativo;
+    }
+
+    public LocalDateTime getDataOperacao() {
+        return dataOperacao;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTipoOperacao(TipoOperacao tipoOperacao) {
+        this.tipoOperacao = tipoOperacao;
+    }
+
+    public void setQuantidade(double quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public void setCriptoativo(Criptoativo criptoativo) {
+        this.criptoativo = criptoativo;
+    }
+
+    public void setDataOperacao(LocalDateTime dataOperacao) {
+        this.dataOperacao = dataOperacao;
+    }
+}
