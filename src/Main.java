0e62b1dd5abf6a89b3249f7a3e382e903a5dcd8a
Ketlyn Santos <ketlyncastro2004@gmail.com>
2026-05-18 23:04:40 -0300

    feat: adiciona try/catch no Main

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        SistemaInvestimentos sistema = new SistemaInvestimentos();
-
-        sistema.iniciar();
+        try {
+            SistemaInvestimentos sistema = new SistemaInvestimentos();
+            sistema.iniciar();
+        } catch (Exception exception) {
+            System.out.println("\nErro no sistema: " + exception.getMessage());
+        }
     }
 }
